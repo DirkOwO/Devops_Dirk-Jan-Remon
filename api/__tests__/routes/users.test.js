ae10d42f44ff87ba4246a1d30604a12bb05c59f9
DirkOwO <dj.schoppema@student.avans.nl>
2024-04-07 23:33:08 +0200

    changed test port

--- api/__tests__/routes/users.test.js
+++ api/__tests__/routes/users.test.js
@@ -5,8 +5,8 @@ const { db, client } = require('../../services/database');
 
 const app = express();
 app.use('/users', usersRouter);
-const server = app.listen(process.env.PORT, () => {
-	console.log(`Express server is listening on port ${process.env.PORT}`);
+const server = app.listen(5000, () => {
+	console.log(`Express server is listening on port 5000`);
 });
 
 describe('Get Users', () => {
